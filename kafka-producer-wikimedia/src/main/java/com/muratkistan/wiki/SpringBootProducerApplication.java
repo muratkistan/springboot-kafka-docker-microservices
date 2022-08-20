946ffe453e794766a44a8a15e6de940594869342
muratkistan <muratkistan@gmail.com>
2022-08-20 15:35:52 +0300

    added commandline runner

--- kafka-producer-wikimedia/src/main/java/com/muratkistan/wiki/SpringBootProducerApplication.java
+++ kafka-producer-wikimedia/src/main/java/com/muratkistan/wiki/SpringBootProducerApplication.java
@@ -1,12 +1,23 @@
 package com.muratkistan.wiki;
 
+import com.muratkistan.wiki.producer.WikimediaProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootProducerApplication {
+public class SpringBootProducerApplication implements CommandLineRunner {
+
+    @Autowired
+    private WikimediaProducer wikimediaProducer;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootProducerApplication.class);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        wikimediaProducer.sendMessage();
     }
 }
