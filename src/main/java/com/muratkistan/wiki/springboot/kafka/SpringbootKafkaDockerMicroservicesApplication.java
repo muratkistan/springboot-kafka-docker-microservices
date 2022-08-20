bde8af2797912c873d1d4d0b9f3eda8937e06238
muratkistan <muratkistan@gmail.com>
2022-08-20 14:12:25 +0300

    First Commit

---
+++ src/main/java/com/muratkistan/wiki/springboot/kafka/SpringbootKafkaDockerMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.muratkistan.wiki.springboot.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaDockerMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaDockerMicroservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/muratkistan/wiki/springboot/kafka/SpringbootKafkaDockerMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muratkistan.wiki.springboot.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaDockerMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
